8d4c843a813aec1bfeae0cfe6364599b138614dd
ijab <zhancaibao@gmail.com>
2012-12-01 15:46:14 -0500

    Make PHP and Javascript works

---
+++ web/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+<title>the title</title>
+   <script type="text/javascript" charset="utf-8" src="script/cordova-2.2.0.js"></script>
+   <script type="text/javascript" src="script/jquery-1.8.2.min.js"></script>
+   <script type="text/javascript" src="script/jquery.mobile-1.2.0.min.js"></script>
+   <script type="text/javascript" src="script/thirdEye.js"></script>
+</head>
+<body>
+   <p>The Third Eye File Sharing Cube</p>
+   <div id="stage" style="background-color: #f2c43d;">
+          STAGE
+   </div>
+   <div id="filelist" style="background-color: #f2c43d;">
+          FileList
+   </div>
+   <input type="button" id="driver" value="Show files" />
+   <input type="button" id="startBump" value="startBump" />
+</body>
+</html>
---
+++ web/list.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    </head>
+    <body>
+        <h1>Available Docs</h1>
+        <?php
+            echo urldecode($_GET["location"]);
+        ?>
+    </body>
+</html>
